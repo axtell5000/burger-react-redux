2e0d96699dda7675219a71c4c4d124048e43c02e
Stephen Axtell <axtell.stephen@gmail.com>
2018-10-05 15:43:43 +0200

    initial commit after working localy

---
+++ src/hoc/Auxy/Auxy.js
@@ -0,0 +1,6 @@
+const auxy = (props) => props.children;
+
+export default auxy;
+
+// A Higher order function that wraps another function. Auxy (cant be Aux on windows). Acts as a root wrapper for other
+// elements
